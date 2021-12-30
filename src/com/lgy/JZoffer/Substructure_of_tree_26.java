366d38f31fa90934b7f9b9f2a6657f4e03ad2ca3
dllgdxlgy <dlutlgy@163.com>
2021-12-30 17:22:36 +0800

    2021-12-24 剑指Offer 21、26

--- src/com/lgy/JZoffer/Substructure_of_tree_26.java
+++ src/com/lgy/JZoffer/Substructure_of_tree_26.java
@@ -7,28 +7,47 @@ import java.util.Stack;
 
 public class Substructure_of_tree_26 {
 
-    public boolean isSubStructure(TreeNode A, TreeNode B) {
+//    public boolean isSubStructure(TreeNode A, TreeNode B) {
+//
+//        if (A == null || B == null)return false;
+//        String resA = inOrderTraverse(A);
+//        String resB = inOrderTraverse(B);
+//        return resA.contains(resB);
+//    }
+//
+//    public String inOrderTraverse(TreeNode root) {
+//        String res = null;
+//        Stack<TreeNode> stack = new Stack<>();
+//        TreeNode node = root;
+//        while (node != null || !stack.isEmpty()) {
+//            if (node != null) {
+//                stack.push(node);
+//                node = node.left;
+//            } else {
+//                TreeNode tem = stack.pop();
+//                res =""+tem.val;
+//                node = tem.right;
+//            }
+//        }
+//        return res;
+//    }
 
-        if (A == null || B == null)return false;
-        String resA = inOrderTraverse(A);
-        String resB = inOrderTraverse(B);
-        return resA.contains(resB);
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+       boolean res = (A != null && B != null) &&( recur(A,B) || isSubStructure(A.left,B) || isSubStructure(A.right,B));
+
+
+        return res;
     }
 
-    public String inOrderTraverse(TreeNode root) {
-        String res = null;
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode node = root;
-        while (node != null || !stack.isEmpty()) {
-            if (node != null) {
-                stack.push(node);
-                node = node.left;
-            } else {
-                TreeNode tem = stack.pop();
-                res =""+tem.val;
-                node = tem.right;
-            }
+    private boolean recur(TreeNode A, TreeNode B) {
+
+        if ( B == null){
+            return  true;
         }
-        return res;
+        if (A == null || A.val!=B.val){
+            return false;
+        }
+        return recur(A.left,B.left) && recur(A.right,B.right);
+
     }
 }
