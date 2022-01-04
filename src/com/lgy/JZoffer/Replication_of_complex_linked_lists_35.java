291fe5b501082758de7383e8fb965a4a8763f411
dllgdxlgy <1069098299@qq.com>
2022-01-04 14:40:26 +0800

    剑指Offer算法 18、35（未做完）

---
+++ src/com/lgy/JZoffer/Replication_of_complex_linked_lists_35.java
@@ -0,0 +1,16 @@
+package com.lgy.JZoffer;
+
+import com.lgy.JZoffer.entity.Node1;
+
+/**
+ * @author LGY
+ * @create 2022-01-04 14:30
+ */
+public class Replication_of_complex_linked_lists_35 {
+
+    public Node1 copyRandomList(Node1 head) {
+
+
+        return null;
+    }
+}
---
+++ src/com/lgy/JZoffer/entity/Node1.java
@@ -0,0 +1,18 @@
+package com.lgy.JZoffer.entity;
+
+/**
+ * @author LGY
+ * @create 2022-01-04 14:31
+ */
+public class Node1 {
+
+    int val;
+    Node next;
+    Node random;
+
+    public Node1(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
