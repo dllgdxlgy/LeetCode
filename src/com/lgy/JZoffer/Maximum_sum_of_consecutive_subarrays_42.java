6f17654f7cf042898195f7304f5c1f2973989e20
dllgdxlgy <1069098299@qq.com>
2022-01-07 22:21:51 +0800

    剑指Offer算法 38、42(未写完).

---
+++ src/com/lgy/JZoffer/Maximum_sum_of_consecutive_subarrays_42.java
@@ -0,0 +1,15 @@
+package com.lgy.JZoffer;
+
+/**
+ * 连续子数组的最大和
+ *
+ * @author LGY
+ * @create 2022-01-07 19:10
+ */
+public class Maximum_sum_of_consecutive_subarrays_42 {
+
+    public int maxSubArray(int[] nums) {
+
+        return 0;
+    }
+}
