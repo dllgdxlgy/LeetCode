f462122deb29bce45aec9be26cf55ddd013975f0
dllgdxlgy <1069098299@qq.com>
2022-01-08 22:08:55 +0800

    剑指Offer算法 33、42(未写完).

---
+++ src/com/lgy/JZoffer/Post_order_traversal_sequence_of_a_binary_search_tree_33.java
@@ -0,0 +1,29 @@
+package com.lgy.JZoffer;
+
+/**
+ * 二叉搜索树的后序遍历序列
+ *
+ * @author LGY
+ * @create 2022-01-08 21:36
+ */
+public class Post_order_traversal_sequence_of_a_binary_search_tree_33 {
+
+    public boolean verifyPostorder(int[] postorder) {
+        return recur(postorder, 0, postorder.length - 1);
+    }
+
+    boolean recur(int[] postorder, int i, int j) {
+
+        if (i >= j) return true;
+        int p = i;
+        while (postorder[p] < postorder[j]) {
+            p++;
+        }
+        int m = p;
+        while (postorder[p] > postorder[j]) {
+            p++;
+        }
+        return p == j && recur(postorder, i, m - 1) && recur(postorder, m , j - 1);
+    }
+
+}
