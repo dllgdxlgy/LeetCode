da296ac9df53c0215b0908478c4fef78550613a0
dllgdxlgy <1069098299@qq.com>
2022-01-16 23:31:03 +0800

    剑指Offer算法 66、42(未写完).

--- src/com/lgy/JZoffer/Building_a_product_array_66.java
+++ src/com/lgy/JZoffer/Building_a_product_array_66.java
@@ -10,6 +10,7 @@ public class Building_a_product_array_66 {
 
     /**
      * 此方法用到了除法，不符合题意。
+     *
      * @param a
      * @return
      */
@@ -30,5 +31,33 @@ public class Building_a_product_array_66 {
 
         return arr;
     }
+
+    /**
+     *
+     * @param a
+     * @return
+     */
+    public int[] constructArr_1(int[] a) {
+
+        if (a.length == 0){
+            return new int[0];
+        }
+        int length = a.length;
+        //创建数组
+        int[] arr = new int[length];
+        int tmp = 1;
+        arr[0] = 1;
+
+        for (int i = 1; i < length; i++) {
+            arr[i] = a[i - 1] * arr[i - 1];
+        }
+        for (int j = length - 2; j >= 0; j--) {
+            tmp *= a[j+1];
+            arr[j] *= tmp;
+        }
+        return arr;
+
+    }
+
 
 }
