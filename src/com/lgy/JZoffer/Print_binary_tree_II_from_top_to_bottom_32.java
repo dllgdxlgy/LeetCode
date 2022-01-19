04e21163e9675d7e5becccad329d3e5c85e24e17
dllgdxlgy <1069098299@qq.com>
2022-01-19 22:16:20 +0800

    Leetcode算法 56、42(未写完).

--- src/com/lgy/JZoffer/Print_binary_tree_II_from_top_to_bottom_32.java
+++ src/com/lgy/JZoffer/Print_binary_tree_II_from_top_to_bottom_32.java
@@ -2,30 +2,38 @@ package com.lgy.JZoffer;
 
 import com.lgy.JZoffer.entity.TreeNode;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
- * 从上到下打印二叉树 II
+ * 从上到下打印二叉树 II (未写完)
  *
  * @author LGY
  * @create 2022-01-18 23:07
  */
 public class Print_binary_tree_II_from_top_to_bottom_32 {
 
+    List<List<Integer>> lists = null;
+    List<Integer> list = null;
     /**
      *
-     *
+     *i
      * @param root
      * @return
      */
     public List<List<Integer>> levelOrder(TreeNode root) {
+        lists = new LinkedList<>();
+        lists = bfs(root);
 
-        List<List<Integer>> lists = new LinkedList<>();
-         if (root == null){
-             return lists;
-         }
-         return null;
+         return lists;
     }
+    public List<List<Integer>> bfs(TreeNode node){
+
+        if (node == null){
+            return lists;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        return null;
+    }
+
 
 }
