841db1391dbf6b35f2840a1ac43de57addcc6f0a
dllgdxlgy <1069098299@qq.com>
2022-02-15 23:19:52 +0800

    Leetcode算法 69

---
+++ src/com/lgy/leetcode/Square_root_of_X_69.java
@@ -0,0 +1,31 @@
+package com.lgy.leetcode;
+
+/**
+ * @author LGY
+ * @create 2022-02-15 23:02
+ */
+public class Square_root_of_X_69 {
+
+    public int mySqrt(int x) {
+
+        if (x == 0) {
+            return 0;
+        }
+
+        int l = 1, r = x;
+        int mid, sqrt;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            sqrt = x / mid;
+            if (sqrt == mid) {
+                return mid;
+            } else if (mid > sqrt) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+
+}
