7858bbc6f4d0831b514d2bd4af6703beead27bd3
dllgdxlgy <1069098299@qq.com>
2022-02-22 23:34:40 +0800

    Leetcode算法 T141

--- src/com/lgy/Hot_100/T141.java
+++ src/com/lgy/Hot_100/T141.java
@@ -1,14 +1,28 @@
 package com.lgy.Hot_100;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author LGY
  * @create 2022-02-22 23:08
  */
 public class T141 {
     public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        Set<ListNode> set = new HashSet<>();
+        ListNode listNode = head;
+        while (listNode != null){
+            if (!set.add(listNode)){
+                return  true;
+            }
+            listNode = listNode.next;
+        }
 
 
-
-        return  false;
+        return false;
     }
 }
