7132a914c30bacd955f6f96c7a3be72532b6e69b
dllgdxlgy <1069098299@qq.com>
2022-02-22 23:13:19 +0800

    Leetcode算法 T141

---
+++ src/com/lgy/Hot_100/T141.java
@@ -0,0 +1,14 @@
+package com.lgy.Hot_100;
+
+/**
+ * @author LGY
+ * @create 2022-02-22 23:08
+ */
+public class T141 {
+    public boolean hasCycle(ListNode head) {
+
+
+
+        return  false;
+    }
+}
