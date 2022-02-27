926e2de0dde47288e4e21b72410d58b9861b0852
dllgdxlgy <dlutlgy@163.com>
2022-02-27 21:17:21 +0800

    T39

--- src/com/lgy/Hot_100/T39.java
+++ src/com/lgy/Hot_100/T39.java
@@ -1,5 +1,22 @@
 package com.lgy.Hot_100;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class T39 {
 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> listList = new ArrayList<>();
+
+        if (len == 0){
+            return listList;
+        }
+
+
+
+
+
+        return null;
+    }
 }
