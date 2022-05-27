395b079f38bfa314fe010258cd477d67a7117520
dllgdxlgy <1069098299@qq.com>
2022-05-27 14:54:42 +0800

    T68

--- src/com/lgy/JZoffer/Duplicate_number_in_array_03.java
+++ src/com/lgy/JZoffer/Duplicate_number_in_array_03.java
@@ -1,5 +1,7 @@
 package com.lgy.JZoffer;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,35 +11,49 @@ import java.util.Set;
  */
 public class Duplicate_number_in_array_03 {
 
-    public int findRepeatNumber1(int[] nums) {
+//    public int findRepeatNumber1(int[] nums) {
+//
+//        Set<Integer> set = new HashSet<>();
+//        for (int num :nums){
+//            if (set.contains(num))return num;
+//            set.add(num);
+//        }
+//        return -1;
+//    }
+//
+//    public int findRepeatNumber2(int[] nums) {
+//
+//        int index = 0;
+//        while (index < nums.length){
+//
+//            // 1. 先判断索引和值是不是相等，相等就可以继续加了
+//            if (nums[index] == index){
+//                index++;
+//                continue;
+//            }
+//            // 2. 如果两个值相等，那就是重复值，可以返回了。
+//            if (nums[nums[index]] == nums[index]){
+//                return nums[index];
+//            }
+//            // 3. 要不就交换
+//            int tmp = nums[index];
+//            nums[index] = nums[tmp];
+//            nums[tmp] = tmp;
+//        }
+//        return -1;
+//    }
 
-        Set<Integer> set = new HashSet<>();
-        for (int num :nums){
-            if (set.contains(num))return num;
-            set.add(num);
-        }
-        return -1;
-    }
+    public int findRepeatNumber(int[] nums) {
 
-    public int findRepeatNumber2(int[] nums) {
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        int index = 0;
-        while (index < nums.length){
-
-            // 1. 先判断索引和值是不是相等，相等就可以继续加了
-            if (nums[index] == index){
-                index++;
-                continue;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])){
+                return nums[i];
             }
-            // 2. 如果两个值相等，那就是重复值，可以返回了。
-            if (nums[nums[index]] == nums[index]){
-                return nums[index];
-            }
-            // 3. 要不就交换
-            int tmp = nums[index];
-            nums[index] = nums[tmp];
-            nums[tmp] = tmp;
+            map.put(nums[i],1);
         }
-        return -1;
+        return 0;
     }
 }
---
+++ src/com/lgy/JZoffer/T68_I.java
@@ -0,0 +1,17 @@
+package com.lgy.JZoffer;
+
+import com.lgy.JZoffer.entity.TreeNode;
+
+/**
+ * @author LGY
+ * @create 2022-05-26 13:44
+ */
+public class T68_I {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+
+        return null;
+
+    }
+}
