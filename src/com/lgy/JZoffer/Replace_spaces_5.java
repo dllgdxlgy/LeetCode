10c14d372398bd615b14208fc6c7c52a6d0d1595
dllgdxlgy <1069098299@qq.com>
2022-05-27 15:29:27 +0800

    T68

--- src/com/lgy/JZoffer/Replace_spaces_5.java
+++ src/com/lgy/JZoffer/Replace_spaces_5.java
@@ -63,11 +63,18 @@ public class Replace_spaces_5 {
         String s1 = r.replaceSpace(s);
     }
 
+    /**
+     *  第二遍刷
+     * @param s
+     * @return
+     */
     public String replaceSpace (String s) {
         // write code here
         char[] sh = s.toCharArray();
+        // 第一次使用的是StringBuilder ，注意别忘记括号。
         StringBuilder str = new StringBuilder();
         for(char c : sh){
+            // 一个字符是单引号，如果是双引号那就是字符串。
             if(c == ' '){
                 str.append("%20");
             }else{
@@ -75,7 +82,22 @@ public class Replace_spaces_5 {
             }
         }
         System.out.println(str);
+
+        // 注意： String.valueOf() , 里面是大写的
         String s1 = String.valueOf(str);
         return s1;
+    }
+
+    public String replaceSpace_4 (String s) {
+        StringBuilder sb = new StringBuilder();
+        int length = s.length();
+        for(int i = 0; i < length; i++ ){
+            if(' ' == s.charAt(i)){
+                sb.append("%20");
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return String.valueOf(sb);
     }
 }
