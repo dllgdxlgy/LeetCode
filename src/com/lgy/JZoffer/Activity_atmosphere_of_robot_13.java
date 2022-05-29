f2518178b93fae38a145a522973ff00cfff37c34
dllgdxlgy <1069098299@qq.com>
2022-05-29 20:48:17 +0800

    T12_JZOffer

--- src/com/lgy/JZoffer/Activity_atmosphere_of_robot_13.java
+++ src/com/lgy/JZoffer/Activity_atmosphere_of_robot_13.java
@@ -27,55 +27,89 @@ public class Activity_atmosphere_of_robot_13 {
 //        return num;
 //    }
 
-    int m,n,k;
-    boolean[][] visited;
+//    int m,n,k;
+//    boolean[][] visited;
+//
+//
+//    /**
+//     *
+//     * @param m
+//     * @param n
+//     * @param k
+//     * @return
+//     */
+//    public int movingCount(int m, int n, int k) {
+//        //赋值
+//        this.m = m;this.n = n;this.k = k;
+//        this.visited = new boolean[m][n];
+//        return dfs(0,0);
+//    }
+//
+//    /**
+//     *
+//     * @param i
+//     * @param j
+//     * @return
+//     */
+//    public int dfs(int i, int j){
+//
+//        int sumi = process_Mumber(i);
+//        int sumj = process_Mumber(j);
+//        if (i >= m || j>=n ||(sumi+sumj)>k || visited[i][j]) return 0;
+//        visited[i][j] = true;
+//        return 1+dfs(i+1,j)+dfs(i,j+1);
+//    }
+//
+//    /**
+//     * 此方法只能处理两位数的 number
+//     *
+//     * @param number
+//     * @return
+//     */
+//    public int process_Mumber(int number) {
+//        int s = number / 10;
+//        int g = number % 10;
+//        int sum = s + g;
+//        return sum;
+//    }
+//
+//    public static void main(String[] args) {
+//        Activity_atmosphere_of_robot_13 a = new Activity_atmosphere_of_robot_13();
+//        int mub = a.movingCount(16, 8, 4);
+//        System.out.println(mub);
+//    }
+
+    int res = 0;
 
 
-    /**
-     *
-     * @param m
-     * @param n
-     * @param k
-     * @return
-     */
-    public int movingCount(int m, int n, int k) {
-        //赋值
-        this.m = m;this.n = n;this.k = k;
-        this.visited = new boolean[m][n];
-        return dfs(0,0);
+    public int movingCount(int threshold, int rows, int cols) {
+        // 创建一个 boolean 数组
+        boolean[][] bool =  new boolean[rows][cols];
+        dfs(threshold,bool,0,0);
+
+        return res;
     }
+    public void dfs(int threshold,boolean[][] bool,int i,int j ){
 
-    /**
-     *
-     * @param i
-     * @param j
-     * @return
-     */
-    public int dfs(int i, int j){
+        if(i < 0 || i >= bool.length || j < 0|| j >= bool[0].length ||bool[i][j]|| threshold<(i%10+i/10+j%10+j/10) ){
+            return;
+        }
+        if (threshold >=(i%10+i/10+j%10+j/10)){
+            bool[i][j] = true;
+            res++;
+        }
 
-        int sumi = process_Mumber(i);
-        int sumj = process_Mumber(j);
-        if (i >= m || j>=n ||(sumi+sumj)>k || visited[i][j]) return 0;
-        visited[i][j] = true;
-        return 1+dfs(i+1,j)+dfs(i,j+1);
-    }
+        dfs(threshold,bool,i,j+1);
+        dfs(threshold,bool,i+1,j);
+        dfs(threshold,bool,i,j-1);
+        dfs(threshold,bool,i-1,j);
 
-    /**
-     * 此方法只能处理两位数的 number
-     *
-     * @param number
-     * @return
-     */
-    public int process_Mumber(int number) {
-        int s = number / 10;
-        int g = number % 10;
-        int sum = s + g;
-        return sum;
+
     }
 
     public static void main(String[] args) {
-        Activity_atmosphere_of_robot_13 a = new Activity_atmosphere_of_robot_13();
-        int mub = a.movingCount(16, 8, 4);
-        System.out.println(mub);
+        Activity_atmosphere_of_robot_13 atmosphereOfRobot13 = new Activity_atmosphere_of_robot_13();
+        int i = atmosphereOfRobot13.movingCount(1, 2, 3);
+        System.out.println(i);
     }
 }
--- src/com/lgy/JZoffer/Number_of_1_in_binary_15.java
+++ src/com/lgy/JZoffer/Number_of_1_in_binary_15.java
@@ -12,15 +12,15 @@ public class Number_of_1_in_binary_15 {
      * @param n
      * @return
      */
-    public int hammingWeight1(int n) {
-
-        int num = 0;
-        while (n != 0){
-            num = num +(n&1);
-            n>>>=1;
-        }
-        return num;
-    }
+//    public int hammingWeight1(int n) {
+//
+//        int num = 0;
+//        while (n != 0){
+//            num = num +(n&1);
+//            n>>>=1;
+//        }
+//        return num;
+//    }
 
 
     /**
@@ -28,13 +28,30 @@ public class Number_of_1_in_binary_15 {
      * @param n
      * @return
      */
+//    public int hammingWeight(int n) {
+//        int res = 0;
+//        while(n != 0) {
+//            res++;
+//            n &= n - 1;
+//        }
+//        return res;
+//    }
     public int hammingWeight(int n) {
+
         int res = 0;
-        while(n != 0) {
-            res++;
-            n &= n - 1;
+        while (n != 0){
+            if ((n & 1)==1){
+                res++;
+                n>>>=1;
+            }
         }
         return res;
     }
+
+    public static void main(String[] args) {
+        Number_of_1_in_binary_15 number = new Number_of_1_in_binary_15();
+        number.hammingWeight(5);
+    }
+
 
 }
