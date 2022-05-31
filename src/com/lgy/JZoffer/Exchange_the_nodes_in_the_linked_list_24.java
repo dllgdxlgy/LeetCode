59a410cc5210bcfca4d50eb090dde38e19f306f5
dllgdxlgy <1069098299@qq.com>
2022-05-31 14:42:04 +0800

    T24_JZOffer

--- src/com/lgy/JZoffer/Exchange_the_nodes_in_the_linked_list_24.java
+++ src/com/lgy/JZoffer/Exchange_the_nodes_in_the_linked_list_24.java
@@ -42,4 +42,42 @@ public class Exchange_the_nodes_in_the_linked_list_24 {
         Exchange_the_nodes_in_the_linked_list_24 wx = new Exchange_the_nodes_in_the_linked_list_24();
         wx.swapPairs(head);
     }
+
+    /**
+     * 牛客 24  2刷
+     * @param head
+     * @return
+     */
+    public ListNode ReverseList(ListNode head) {
+        ListNode node = head;
+        int len = 0;
+        while(node != null){
+            len++;
+            node = node.next;
+        }
+        int  left = 1;
+        int right = len;
+        int tem_v = -1;
+        while(left < right){
+            int l = returnNodeByNum(head,left).val;
+            int r = returnNodeByNum(head,right).val;
+            tem_v = l;
+            returnNodeByNum(head,left).val=r;
+            returnNodeByNum(head,right).val = tem_v;
+            left++;
+            right--;
+        }
+        return head;
+
+    }
+
+    public ListNode returnNodeByNum(ListNode head,int num){
+        ListNode temp = head;
+        int i = 1;
+        while(i < num){
+            temp = temp.next;
+            i++;
+        }
+        return temp;
+    }
 }
