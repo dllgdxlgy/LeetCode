8dd7a66db9b2bf80f5ffe2cdae233b5fcbe39935
dllgdxlgy <1069098299@qq.com>
2022-06-03 19:35:18 +0800

    T33

--- src/com/lgy/JZoffer/Post_order_traversal_sequence_of_a_binary_search_tree_33.java
+++ src/com/lgy/JZoffer/Post_order_traversal_sequence_of_a_binary_search_tree_33.java
@@ -26,4 +26,38 @@ public class Post_order_traversal_sequence_of_a_binary_search_tree_33 {
         return p == j && recur(postorder, i, m - 1) && recur(postorder, m , j - 1);
     }
 
+    /**
+     * 二遍
+     * @param sequence
+     * @return
+     */
+
+    public boolean verifyPostorder_1(int [] sequence) {
+
+
+        int right = sequence.length-1;
+        return houxu(sequence,0,right);
+
+    }
+
+    public boolean houxu(int[] sequence,int left,int right){
+        if(left >= right){
+            return true;
+        }
+        int root = sequence[right];
+
+        int i = left;
+        while(sequence[i]<root){
+            i++;
+        }
+        int j = i;
+        while(i<right){
+            if(sequence[i]<root){
+                return false;
+            }
+            i++;
+        }
+        return houxu(sequence,left,j-1)&& houxu(sequence,j,right-1);
+
+    }
 }
