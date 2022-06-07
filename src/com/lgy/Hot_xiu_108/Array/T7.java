db80e7e8b79d66650b60d7f96e633a24eb178dd8
dllgdxlgy <1069098299@qq.com>
2022-06-07 15:37:19 +0800

    T7

---
+++ src/com/lgy/Hot_xiu_108/Array/T7.java
@@ -0,0 +1,46 @@
+package com.lgy.Hot_xiu_108.Array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ *7. 数组中出现次数超过一半的数字
+ https://www.nowcoder.com//
+ practice/e8a1b01a2df14cb2b228b30ee6a92
+ 163?tpId=190&&tqId=35371&rp=1&ru=/activity/
+ oj&qru=/ta/job-code-high-rd/question-ranking
+ * @author LGY
+ * @create 2022-06-07 15:24
+ */
+public class T7 {
+
+    /**
+     * 自己做的
+     * @param array
+     * @return
+     */
+    public int MoreThanHalfNum_Solution(int [] array) {
+        Arrays.sort(array);
+        int r = array.length;
+        return array[r/2];
+    }
+
+    /**
+     * 借助 HashMap
+     * @param array
+     * @return
+     */
+    public int MoreThanHalfNum_Solution_1(int [] array) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int len = array.length;
+        for(int i = 0; i < len ; i++){
+            map.put(array[i],map.getOrDefault(array[i],0)+1);
+        }
+        for(Integer t :map.keySet()){
+            if(map.get(t)>len/2){
+                return t.intValue();
+            }
+        }
+        return -1;
+    }
+}
