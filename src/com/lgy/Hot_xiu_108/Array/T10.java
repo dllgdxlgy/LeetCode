ea89af75f671c7181234552ef37df2a73582cfb9
dllgdxlgy <1069098299@qq.com>
2022-06-08 23:37:30 +0800

    T10

---
+++ src/com/lgy/Hot_xiu_108/Array/T10.java
@@ -0,0 +1,15 @@
+package com.lgy.Hot_xiu_108.Array;
+
+/**
+ *10. 缺失的第一个正整数
+ https://www.nowcoder.com//practice/50ec6a5b0e4e45348544348278cdcee5?tpId=190&rp=1&ru=%2Factivity%2Foj&qru=%2Fta%2Fjo
+ b-code-high-rd%2Fquestion-ranking&difficulty=&judgeStatus=&tags=&title=%E6%AD%A3%E6%95%B4%E6%95%B0&sourceUrl=&gioEnter=menu
+ * @author LGY
+ * @create 2022-06-07 16:30
+ */
+public class T10 {
+
+
+
+
+}
