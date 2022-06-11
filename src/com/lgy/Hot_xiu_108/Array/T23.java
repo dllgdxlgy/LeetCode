18ec36730f7bc91b3c05ea1af1446bb384da0e83
dllgdxlgy <1069098299@qq.com>
2022-06-11 22:40:00 +0800

    T23

--- src/com/lgy/Hot_xiu_108/Array/T23.java
+++ src/com/lgy/Hot_xiu_108/Array/T23.java
@@ -10,4 +10,32 @@ package com.lgy.Hot_xiu_108.Array;
  */
 public class T23 {
 
+
+    /**
+     * 自己做的
+     * @param grid
+     * @return
+     */
+
+    public int minPathSum (int[][] grid) {
+        // write code here
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] res = new int[n][m];
+        res[0][0] = grid[0][0];
+        for(int i = 1; i < m ;i++){
+            res[0][i] = grid[0][i] + res[0][i-1];
+        }
+        for(int i = 1; i < n; i++){
+            res[i][0] = grid[i][0] + res[i-1][0];
+        }
+        if(n >= 2 && m >= 2){
+            for(int i = 1; i < n; i++){
+                for(int j = 1; j< m; j++){
+                    res[i][j] = Math.min(res[i-1][j],res[i][j-1])+grid[i][j];
+                }
+            }
+        }
+        return res[n-1][m-1];
+    }
 }
