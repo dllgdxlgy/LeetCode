e751524c2e1e16428d1cbb073320a4653c927b37
dllgdxlgy <1069098299@qq.com>
2022-06-20 10:49:02 +0800

    T8

--- src/com/lgy/Hot_xiu_108/DP/T12.java
+++ src/com/lgy/Hot_xiu_108/DP/T12.java
@@ -39,13 +39,51 @@ public class T12 {
                     dp[i][j] = dp[i-1][j-1];
                 }else {
                     int replace = dp[i-1][j-1] +1;
-                    int delete = dp[i][j-1] + 1;
-                    int insert = dp[i-1][j] + 1;
+                    int delete = dp[i-1][j] + 1;
+                    int insert = dp[i][j-1] + 1;
                     dp[i][j] = Math.min(replace,Math.min(delete,insert));
                 }
             }
         }
 
         return dp[len_word1][len_word2];
+    }
+
+    /**
+     * 编辑代价 二
+     * @param str1
+     * @param str2
+     * @param ic
+     * @param dc
+     * @param rc
+     * @return
+     */
+    public int minEditCost (String str1, String str2, int ic, int dc, int rc) {
+        // write code here
+        int len_str1 = str1.length();
+        int len_str2 = str2.length();
+
+        int[][] dp = new int[len_str1+1][len_str2+1];
+
+        for (int i = 1; i <= len_str2; i++) {
+            dp[0][i] = i * ic;
+        }
+        for (int i = 1; i <= len_str1; i++) {
+            dp[i][0] = i *dc;
+        }
+
+        for (int i = 1; i <= len_str1; i++) {
+            for (int j = 1; j <= len_str2; j++) {
+                if (str1.charAt(i-1) == str2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else {
+                    int delete = dp[i-1][j] +dc;
+                    int insert = dp[i][j-1] +ic;
+                    int replace = dp[i-1][j-1] +rc;
+                    dp[i][j] = Math.min(replace,Math.min(delete,insert));
+                }
+            }
+        }
+        return dp[len_str1][len_str2];
     }
 }
