6d979bdb3c2cd429b39cf5ee0a537adc15ba49ce
dllgdxlgy <1069098299@qq.com>
2022-06-24 15:10:00 +0800

    最大深度——层次

--- src/com/lgy/Hot_xiu_108/DFS/T3.java
+++ src/com/lgy/Hot_xiu_108/DFS/T3.java
@@ -2,6 +2,9 @@ package com.lgy.Hot_xiu_108.DFS;
 
 import com.lgy.JZoffer.entity.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * T3 .二叉树的最大深度
 
@@ -25,5 +28,35 @@ public class T3 {
         }
         return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
 
+    }
+
+    /**
+     * 二叉树的最大深度
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth_1 (TreeNode root) {
+        // write code here
+        if(root == null){
+            return 0 ;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        int res = 0;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size;i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            res++;
+        }
+        return res;
     }
 }
