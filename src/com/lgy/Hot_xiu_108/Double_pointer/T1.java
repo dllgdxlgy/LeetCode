16eba1be97e897e0e7029be5a287099b88aea548
dllgdxlgy <1069098299@qq.com>
2022-06-30 14:24:27 +0800

    LeetCode-T11_划分链表

---
+++ src/com/lgy/Hot_xiu_108/Double_pointer/T1.java
@@ -0,0 +1,9 @@
+package com.lgy.Hot_xiu_108.Double_pointer;
+
+/**
+ * @author LGY
+ * @create 2022-06-28 22:22
+ */
+public class T1 {
+
+}
---
+++ src/com/lgy/Hot_xiu_108/Double_pointer/T11.java
@@ -0,0 +1,55 @@
+package com.lgy.Hot_xiu_108.Double_pointer;
+
+import com.lgy.JZoffer.entity.ListNode;
+
+/**
+ * T11. 划分链表
+ https://www.nowcoder.com/practice/1dc1036be38f45f19000e48abe00b12f?tpId=190&&tqId=3
+ 6639&rp=1&ru=/activity/oj&qru=/ta/job-code-high-rd/question-ranking
+ * @author LGY
+ * @create 2022-06-28 22:25
+ */
+public class T11 {
+
+    public ListNode partition (ListNode head, int x) {
+        // write code here
+
+        ListNode root = new ListNode(0);
+        ListNode temp = root;
+        ListNode node = head;
+        while(node != null){
+            if(node.val < x){
+                temp.next = new ListNode(node.val);
+                temp = temp.next;
+            }
+            node = node.next;
+        }
+        node = head;
+        while(node != null){
+            if(node.val >= x){
+                temp.next = new ListNode(node.val);
+                temp = temp.next;
+            }
+            node = node.next;
+        }
+        return root.next;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode root = new ListNode(1);
+        ListNode node = root;
+        root.next = new ListNode(4);
+        root = root.next;
+        root.next=new ListNode(3);
+        root = root.next;
+        root.next=new ListNode(2);
+        root = root.next;
+        root.next=new ListNode(5);
+        root = root.next;
+        root.next=new ListNode(2);
+        T11 t11 = new T11();
+        t11.partition(node,3);
+
+    }
+}
