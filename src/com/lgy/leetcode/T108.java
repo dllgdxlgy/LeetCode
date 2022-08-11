52c0f6835e31fb1366474fadcfb2e41f4ebd63d2
dllgdxlgy <1069098299@qq.com>
2022-08-11 09:35:06 +0800

    T108 数组转二叉树

--- src/com/lgy/leetcode/T108.java
+++ src/com/lgy/leetcode/T108.java
@@ -22,4 +22,30 @@ public class T108 {
         T108 t108 = new T108();
         TreeNode node = t108.sortedArrayToBST(arr);
     }
+
+
+    /**
+     * 第二遍
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST_1(int[] nums) {
+
+        return ArraytoBST(nums,0,nums.length-1);
+
+    }
+
+    public TreeNode ArraytoBST(int[] arr, int i, int j){
+        int left = i;
+        int right = j;
+        if(left > right){
+            return null;
+        }
+        int temp = (left + right)/2;
+        TreeNode root = new TreeNode(arr[temp]);
+        root.left = ArraytoBST(arr,left,temp-1);
+        root.right = ArraytoBST(arr,temp+1,right);
+
+        return root;
+    }
 }
