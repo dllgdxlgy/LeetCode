55e713b6c2608e810d56f917bf472a077a8e49b2
dllgdxlgy <1069098299@qq.com>
2022-08-16 16:32:54 +0800

    T96. 不同的二叉搜索树

--- src/com/lgy/leetcode/T96.java
+++ src/com/lgy/leetcode/T96.java
@@ -1,7 +1,32 @@
 package com.lgy.leetcode;
 
+/**
+ *  T96. 不同的二叉搜索树
+ */
 public class T96 {
+
+    /**
+     * 二刷
+     * @param n
+     * @return
+     */
     public int numTrees(int n) {
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2;i < n+1;i++){
+            for(int j = 1; j<=i;j++){
+                dp[i]+=dp[i-j]*dp[j-1];
+            }
+        }
+        return dp[n];
+    }
+
+
+    public int numTrees_1(int n) {
         int[] arr = new int[n+1];
         arr[0] = 1;
         arr[1] = 1;
