896e442b4b7bd9d8bcec3159e140cd765e1309b0
dllgdxlgy <1069098299@qq.com>
2022-09-04 23:06:57 +0800

    无重复字符的最长子串

--- src/com/lgy/CodeTop_100/T3.java
+++ src/com/lgy/CodeTop_100/T3.java
@@ -20,7 +20,7 @@ public class T3 {
      */
     public int lengthOfLongestSubstring(String s) {
 
-        HashMap<Character,Integer> window = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
 
         int left = 0;
         int right = 0;
@@ -29,20 +29,20 @@ public class T3 {
 
         int res = 0;
         // 右指针不出界就行
-        while(right < s.length()){
+        while (right < s.length()) {
             // 取出右节点
             char c = s.charAt(right);
             right++;
-            window.put(c,window.getOrDefault(c,0)+1);
+            window.put(c, window.getOrDefault(c, 0) + 1);
 
             // 注意这里是Integer 的类型，需要使用intValue() 进行转换
-            while(window.get(c).intValue() > 1){
+            while (window.get(c).intValue() > 1) {
                 char d = s.charAt(left);
                 left++;
-                window.put(d,window.getOrDefault(d,1)-1);
+                window.put(d, window.getOrDefault(d, 1) - 1);
             }
 
-            res = Math.max(res,right-left);
+            res = Math.max(res, right - left);
         }
         return res;
     }
@@ -51,8 +51,36 @@ public class T3 {
         T3 t3 = new T3();
         String s = "pwwkew";
         t3.lengthOfLongestSubstring(s);
+    }
 
+    /**
+     * 第二遍———无重复的最长子串
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring_2(String s) {
 
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int len = s.length();
+        //这里是预防空串
+        int res = 0;
+        while (right < len) {
+
+            char temp = s.charAt(right);
+            right++;
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
+
+            while (map.get(temp) > 1) {
+                char left_char = s.charAt(left);
+                left++;
+                map.put(left_char, map.get(left_char) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
     }
 
 
