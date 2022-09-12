2e89cf1794c51a8161a8206fc6b2e715200e468d
dllgdxlgy <1069098299@qq.com>
2022-09-12 13:56:26 +0800

    螺旋矩阵

--- src/com/lgy/CodeTop_100/T54.java
+++ src/com/lgy/CodeTop_100/T54.java
@@ -51,6 +51,49 @@ public class T54 {
             left++;
 
         }
+    }
 
+    /**
+     *
+     * 二刷
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder_2(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length-1;
+        int top = 0;
+        int bottom = matrix.length-1;
+
+        while(left <= right && top <= bottom){
+            if(top <= bottom){
+                for(int i = left; i <= right; i++){
+                    res.add(matrix[top][i]);
+                }
+                top++;
+            }
+
+            if(left <= right){
+                for(int i = top;i <= bottom; i++){
+                    res.add(matrix[i][right]);
+                }
+                right--;
+            }
+            if(top<=bottom){
+                for(int i = right;i>=left;i--){
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left <= right){
+                for(int i = bottom;i>=top;i--){
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
     }
 }
