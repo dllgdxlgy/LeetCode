9c22d5588a1922cb0fa4e17719ebf90bb8fcf413
dllgdxlgy <1069098299@qq.com>
2022-09-12 15:27:08 +0800

    反转指定范围的链表

--- src/com/lgy/CodeTop_100/T92.java
+++ src/com/lgy/CodeTop_100/T92.java
@@ -101,4 +101,54 @@ public class T92 {
         }
         return pre;
     }
+
+    /**
+     * 三刷
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween_3(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode next = dummy;
+        int left_num = 0;
+        int right_num = 0;
+
+        while(left_num < left-1){
+            pre = pre.next;
+            left_num++;
+        }
+        ListNode start = pre.next;
+        right_num = left_num;
+        next = pre;
+        while(right_num < right){
+            next = next.next;
+            right_num++;
+        }
+        ListNode end = next;
+        next = next.next;
+        pre.next = null;
+        end.next = null;
+
+        pre.next = reverse_3(start);
+        start.next = next;
+        return dummy.next;
+
+    }
+
+    public ListNode reverse_3(ListNode root){
+        ListNode pre = null;
+        ListNode end = null;
+
+        while(root != null){
+            end = root;
+            root = root.next;
+            end.next = pre;
+            pre = end;
+        }
+        return pre;
+    }
 }
