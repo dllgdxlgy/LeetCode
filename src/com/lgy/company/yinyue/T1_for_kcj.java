7beb553293254dbc53f8a1f43d9ff9df377c7161
dllgdxlgy <1069098299@qq.com>
2022-09-27 21:55:42 +0800

    T1——Al

--- src/com/lgy/company/yinyue/T1_for_kcj.java
+++ src/com/lgy/company/yinyue/T1_for_kcj.java
@@ -1,5 +1,8 @@
 package com.lgy.company.yinyue;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author LGY
  * @create 2022-09-26 19:18
@@ -46,13 +49,48 @@ public class T1_for_kcj {
 //        return res;
 //    }
 
-    public int res(int[] arr, int k) {
-        int num = (int) Math.pow(10, k);
+//    public int res(int[] arr, int k) {
+//        int num = (int) Math.pow(10, k);
+//        int res = 0;
+//        int n = arr.length;
+//        //起始位置
+//        for (int i = 0; i < n; i++) {
+//            int sum = 1;
+//            for (int j = i; j < n; j++) {
+//
+//                sum *= arr[j];
+//                if (sum % num == 0) {
+//                    res += (n - j);
+//                    break;
+//                }
+//            }
+//        }
+//        return res % 1000000007;
+//    }
+
+    public static void main(String[] args) {
+
+        T1_for_kcj t1_for_kcj = new T1_for_kcj();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(2);
+        list.add(3);
+        list.add(50);
+        list.add(4);
+
+        int res = t1_for_kcj.res_1(list, 2);
+        System.out.println(res);
+
+    }
+
+    public int res_1(ArrayList<Integer> list, int k) {
+        int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
+        long num = (long) Math.pow(10, k);
         int res = 0;
         int n = arr.length;
         //起始位置
         for (int i = 0; i < n; i++) {
-            int sum = 1;
+            long sum = 1;
             for (int j = i; j < n; j++) {
 
                 sum *= arr[j];
@@ -63,12 +101,5 @@ public class T1_for_kcj {
             }
         }
         return res % 1000000007;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{5, 2, 3, 50, 4};
-        T1_for_kcj t1_for_kcj = new T1_for_kcj();
-        int res = t1_for_kcj.res(arr, 2);
-        System.out.println(res);
     }
 }
---
+++ src/com/lgy/company/yinyue/T2_for_kcj.java
@@ -0,0 +1,49 @@
+package com.lgy.company.yinyue;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @author LGY
+ * @create 2022-09-27 19:47
+ */
+public class T2_for_kcj {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+            set.add(arr[i]);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        scanner.close();
+
+        int num = 0;
+        for (Integer i : map.keySet()) {
+
+            if (map.get(i) == 1) {
+                continue;
+            } else {
+                int temp = i;
+                int temp_num = 0;
+                while (set.contains(temp - k) && set.contains(temp + k)) {
+                    temp -= k;
+                    temp_num++;
+                }
+                set.add((temp_num+1) * k + i);
+                map.put(i,map.getOrDefault(i,0)-1);
+                num += temp_num == 0 ? 1 : temp_num;
+            }
+        }
+        System.out.println(num);
+
+    }
+}
