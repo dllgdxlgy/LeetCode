20304151b997e18d1d00eb560b83253811026ba3
dllgdxlgy <1069098299@qq.com>
2022-10-04 20:32:58 +0800

    704_二分查找

--- src/com/lgy/leetcode/T704.java
+++ src/com/lgy/leetcode/T704.java
@@ -1,7 +1,6 @@
 package com.lgy.leetcode;
 
 /**
- *
  * 二分查找
  *
  * @author LGY
@@ -13,10 +12,10 @@ public class T704 {
         int res = -1;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target){
+            if (nums[i] == target) {
                 return i;
             }
-            if (nums[i] > target){
+            if (nums[i] > target) {
                 return res;
             }
         }
@@ -25,28 +24,51 @@ public class T704 {
     }
 
     /**
-     *  二分查找
-     *
+     * 二分查找
      */
     public int search_1(int[] nums, int target) {
 
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
 
-        while (left <= right){
+        while (left <= right) {
 
-            int midle = left + (right - left)/2;
-            if (nums[midle] == target){
+            int midle = left + (right - left) / 2;
+            if (nums[midle] == target) {
                 return midle;
-            }else if (nums[midle] < target){
-                left = midle +1;
-            }else if (nums[midle] > target){
-                right = midle -1;
+            } else if (nums[midle] < target) {
+                left = midle + 1;
+            } else if (nums[midle] > target) {
+                right = midle - 1;
             }
 
         }
         return -1;
+    }
 
+
+    /**
+     * 二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search_2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while(left <= right){
+            int temp = left + (right - left)/2;
+            if(nums[temp] == target){
+                return temp;
+            }else if(nums[temp] > target){
+                right = temp-1;
+            }else{
+                left = temp+1;
+            }
+        }
+        return -1;
     }
 
 
