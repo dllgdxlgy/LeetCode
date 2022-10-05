d6b4b9f1d12bc63f9a06e8a2ec989027e9aeae4d
dllgdxlgy <1069098299@qq.com>
2022-10-05 09:53:23 +0800

    T27

--- src/com/lgy/leetcode/Finds_the_first_and_last_positions_of_an_element_in_a_sorted_array_34.java
+++ src/com/lgy/leetcode/Finds_the_first_and_last_positions_of_an_element_in_a_sorted_array_34.java
@@ -6,6 +6,54 @@ package com.lgy.leetcode;
  */
 public class Finds_the_first_and_last_positions_of_an_element_in_a_sorted_array_34 {
 
+    /**
+     * 二刷，好理解
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange_2(int[] nums, int target) {
+
+        // 先给出一个索引位置为 0 的
+        int index = -1;
+
+        int left = 0;
+        int right = nums.length-1;
+
+        // 找出第一个 为目标元素的位置
+        while(left <= right){
+            int temp = left + (right - left)/2;
+            if(nums[temp] == target){
+                index = temp;
+                break;
+            }else if(nums[temp] < target){
+                left = temp+1;
+            }else{
+                right = temp-1;
+            }
+        }
+        // 如果检查以后还为 -1；说明没有元素，直接返回。
+        if(index == -1){
+            return new int[]{-1,-1};
+        }
+
+        //说明有元素，则利用 left、right 进行判断最左和最右两边。
+        left = index;
+        right = index;
+
+        while(left >= 0 && nums[left] == target){
+            left--;
+        }
+        left++;
+        while(right < nums.length && nums[right] == target){
+            right++;
+        }
+        right--;
+
+        //直接返回结果。
+        return new int[]{left,right};
+    }
+
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[]{-1,-1};
         if (nums == null){
@@ -58,45 +106,5 @@ public class Finds_the_first_and_last_positions_of_an_element_in_a_sorted_array_
     }
 
 
-    /**
-     * 二刷
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] searchRange_2(int[] nums, int target) {
 
-        int index = -1;
-
-        int left = 0;
-        int right = nums.length-1;
-
-        while(left <= right){
-            int temp = left + (right - left)/2;
-            if(nums[temp] == target){
-                index = temp;
-                break;
-            }else if(nums[temp] < target){
-                left = temp+1;
-            }else{
-                right = temp-1;
-            }
-        }
-
-        if(index == -1){
-            return new int[]{-1,-1};
-        }
-        left = index;
-        right = index;
-
-        while(left >= 0 && nums[left] == target){
-            left--;
-        }
-        left++;
-        while(right < nums.length && nums[right] == target){
-            right++;
-        }
-        right--;
-        return new int[]{left,right};
-    }
 }
--- src/com/lgy/leetcode/T27.java
+++ src/com/lgy/leetcode/T27.java
@@ -5,6 +5,27 @@ package com.lgy.leetcode;
  * @create 2022-05-04 20:51
  */
 public class T27 {
+
+    /**
+     * 二刷
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement_1(int[] nums, int val) {
+
+        int slow = 0;
+        int fast = 0;
+
+        while(fast < nums.length){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
     public int removeElement(int[] nums, int val) {
         int length = nums.length;
         if (length == 0){
