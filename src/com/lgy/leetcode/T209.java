239ff1b42c43131f51885c35a11e57edcb1139bd
dllgdxlgy <1069098299@qq.com>
2022-10-06 16:17:17 +0800

    T209 二刷、滑动窗口

--- src/com/lgy/leetcode/T209.java
+++ src/com/lgy/leetcode/T209.java
@@ -5,6 +5,39 @@ import com.lgy.company.leetcodecom.T;
 import java.util.Arrays;
 
 public class T209 {
+
+    /**
+     * 二刷、滑动窗口
+     *
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen_1(int target, int[] nums) {
+
+        int res = Integer.MAX_VALUE;
+
+        int left = 0;
+        int right = 0;
+
+        int length = nums.length;
+        int sum = 0;
+        while(right < length){
+            int temp = nums[right];
+            sum += temp;
+            right++;
+
+            while( sum >= target){
+
+                res = Math.min(res,right-left);
+                int temp_left = nums[left];
+                sum-=temp_left;
+                left++;
+            }
+
+        }
+        return res==Integer.MAX_VALUE?0:res;
+    }
     public int minSubArrayLen(int target, int[] nums) {
 
         int arr_min = Integer.MAX_VALUE;
