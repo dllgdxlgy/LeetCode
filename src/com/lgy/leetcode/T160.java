22e48078f8609f278d3f60acc901e4067438645f
dllgdxlgy <1069098299@qq.com>
2022-10-06 21:39:15 +0800

    T160、相交链表

--- src/com/lgy/leetcode/T160.java
+++ src/com/lgy/leetcode/T160.java
@@ -4,6 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class T160 {
+
+
+    /**
+     * T160、相交链表
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode_1(ListNode headA, ListNode headB) {
+
+        ListNode temp_A = headA;
+        ListNode temp_B = headB;
+
+        while(temp_A != temp_B){
+            temp_A = temp_A == null?headB:temp_A.next;
+            temp_B = temp_B == null?headA:temp_B.next;
+        }
+        return temp_A;
+    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
         if (headA == null || headB == null){
