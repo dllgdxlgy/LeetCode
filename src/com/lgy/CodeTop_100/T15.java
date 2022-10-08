ce163a2a9e47df485e6330ccea01412915ecfb80
dllgdxlgy <1069098299@qq.com>
2022-10-08 15:24:41 +0800

    三数之和

--- src/com/lgy/CodeTop_100/T15.java
+++ src/com/lgy/CodeTop_100/T15.java
@@ -14,6 +14,51 @@ import java.util.List;
  */
 public class T15 {
 
+
+    /**
+     * 三刷
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum_1(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0)break;;
+
+            if(i > 0 && nums[i] == nums[i-1])continue;
+
+            int left = i+1;
+            int right = nums.length-1;
+            int temp = nums[i];
+
+            while(left < right){
+                int sum = temp + nums[left]+nums[right];
+                if(sum == 0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[left]);
+                    list.add(nums[i]);
+                    list.add(nums[right]);
+                    res.add(list);
+                    while(left < right && nums[left+1] == nums[left])++left;
+                    while(left < right && nums[right-1] == nums[right])--right;
+                    left++;
+                    right--;
+                }else if ( sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+
+            }
+
+        }
+        return res;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> listList = new ArrayList<>();
         Arrays.sort(nums);
