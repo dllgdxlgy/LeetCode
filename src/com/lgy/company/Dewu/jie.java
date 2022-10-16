ea28aea37e814dfa30ce2d67b25cb1b710ae69be
dllgdxlgy <1069098299@qq.com>
2022-10-16 16:49:37 +0800

    niuke

---
+++ src/com/lgy/company/Dewu/jie.java
@@ -0,0 +1,10 @@
+package com.lgy.company.Dewu;
+
+/**
+ * @author LGY
+ * @create 2022-10-15 15:35
+ */
+public interface jie {
+
+
+}
---
+++ src/com/lgy/company/niuke/T1.java
@@ -0,0 +1,43 @@
+package com.lgy.company.niuke;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @author LGY
+ * @create 2022-10-16 11:11
+ */
+public class T1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(i, scanner.nextInt());
+        }
+
+        scanner.close();
+        int num = 0;
+
+        for(int key : map.keySet()){
+
+            int temp = map.get(key);
+            if(temp >= x){
+                num += temp;
+                continue;
+            }
+            int addtemp = temp + k -1;
+            if(addtemp < x){
+                continue;
+            }else {
+                num += x;
+            }
+        }
+        System.out.println(num);
+    }
+}
