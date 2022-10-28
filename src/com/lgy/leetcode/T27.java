cd431d0600d9ea3e4f68ad4b9fdf2b1d11fe1839
dllgdxlgy <1069098299@qq.com>
2022-10-28 21:33:44 +0800

    T27

--- src/com/lgy/leetcode/T27.java
+++ src/com/lgy/leetcode/T27.java
@@ -6,6 +6,31 @@ package com.lgy.leetcode;
  */
 public class T27 {
 
+
+    /**
+     * 三刷
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement_2(int[] nums, int val) {
+        int slow = 0;
+        int fast = 0;
+
+        while(fast < nums.length){
+            if(nums[fast] == val){
+                fast++;
+            }else {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            }
+        }
+
+        return slow;
+    }
+
+
     /**
      * 二刷
      * @param nums
