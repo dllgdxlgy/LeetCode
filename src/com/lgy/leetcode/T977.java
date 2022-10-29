41c16c13fbc78a4a8d1e35ef79f18cfffdf52bac
dllgdxlgy <1069098299@qq.com>
2022-10-29 20:29:54 +0800

    T977

--- src/com/lgy/leetcode/T977.java
+++ src/com/lgy/leetcode/T977.java
@@ -49,4 +49,30 @@ public class T977 {
         Arrays.sort(arr);
         return arr;
     }
+
+
+    /**
+     * 二刷
+     * @param nums
+     * @return
+     */
+    public int[] sortedSquares_2(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int[] res = new int[nums.length];
+        int index = nums.length-1;
+
+        while(left <= right){
+            if(nums[left] * nums[left] <= nums[right]*nums[right]){
+                res[index] = nums[right]*nums[right];
+                right--;
+            }else{
+                res[index] = nums[left] * nums[left];
+                left++;
+            }
+            index--;
+        }
+        return res;
+    }
 }
