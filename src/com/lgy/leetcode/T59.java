87fc6118dd25b58709b784de89d91c5d59d97543
dllgdxlgy <1069098299@qq.com>
2022-10-29 21:12:51 +0800

    T59

--- src/com/lgy/leetcode/T59.java
+++ src/com/lgy/leetcode/T59.java
@@ -4,6 +4,71 @@ public class T59 {
 
 
     /**
+     * 三刷
+     * @param n
+     * @return
+     */
+    public int[][] generateMatrix_3(int n) {
+
+        int[][] res = new int[n][n];
+
+        int left = 0;
+        int top = 0;
+        int right = n-1;
+        int bottom = n-1;
+        int num = 1;
+
+        while(true){
+            if(left <= right){
+                for(int i = left;i <= right;i++){
+                    res[top][i] = num;
+                    num++;
+                }
+                top++;
+                if(top > bottom){
+                    break;
+                }
+            }
+
+            if(top <= bottom){
+                for(int i = top; i <= bottom; i++){
+                    res[i][right] = num;
+                    num++;
+                }
+                right--;
+                if(right<left){
+                    break;
+                }
+            }
+            if(left <= right){
+                for(int i = right;i >= left;i--){
+                    res[bottom][i] = num;
+                    num++;
+                }
+                bottom--;;
+                if(top > bottom){
+                    break;
+                }
+            }
+
+            if(top <= bottom){
+                for(int i = bottom; i >= top; i--){
+                    res[i][left] = num;
+                    num++;
+                }
+                left++;
+
+                if(right<left){
+                    break;
+                }
+            }
+
+        }
+        return res;
+
+    }
+
+    /**
      * 二刷
      * @param n
      * @return
